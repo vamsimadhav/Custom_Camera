37658029c71c43913e2671db2b57f9a6efdc928f
vamsimadhav <vamsimadhav2000@gmail.com>
2023-03-30 01:42:51 +0530

    Added Shared Prefs for Screen1

--- app/src/main/java/com/example/custom_camera/Fragments/FragmentScreenOne.java
+++ app/src/main/java/com/example/custom_camera/Fragments/FragmentScreenOne.java
@@ -1,6 +1,8 @@
 package com.example.custom_camera.Fragments;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.core.content.ContextCompat;
@@ -48,22 +50,23 @@ public class FragmentScreenOne extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_screen_one, container, false);
         DatabaseHelper databaseHelper = DatabaseHelper.getDB(getContext());
 
+        SharedPreferences preferences = getContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
         nameInput = rootView.findViewById(R.id.userNameTextLayout);
         emailInput = rootView.findViewById(R.id.userEmailTextLayout);
         Button testButton = rootView.findViewById(R.id.testButton);
+
 
         EditText nameEdit = nameInput.getEditText();
         assert nameEdit != null;
         EditText emailEdit = emailInput.getEditText();
         assert emailEdit != null;
 
-        ArrayList<UserData> userArrayData = (ArrayList<UserData>) databaseHelper.userDataDAO().getUserData();
-        if (userArrayData.size() > 0) {
-            String name = userArrayData.get(0).getName();
-            String email = userArrayData.get(0).getEmail();
+            String credentials [] = getCredentials(preferences);
+            String name = credentials[1];
+            String email = credentials[0];
             emailEdit.setText(email);
             nameEdit.setText(name);
-        }
 
         nameEdit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -115,6 +118,7 @@ public class FragmentScreenOne extends Fragment {
                 } else {
                     if (userArrayData.isEmpty()) {
                         UserData userData = new UserData(name,email);
+                        saveCredentials(email,name,preferences);
                         databaseHelper.userDataDAO().addUserData(userData);
                     } else {
                         boolean alreadyExists = false;
@@ -125,6 +129,7 @@ public class FragmentScreenOne extends Fragment {
                         }
                         if (!alreadyExists) {
                             UserData userData = new UserData(name,email);
+                            saveCredentials(email,name,preferences);
                             databaseHelper.userDataDAO().addUserData(userData);
                         }
                     }
@@ -136,8 +141,15 @@ public class FragmentScreenOne extends Fragment {
         return rootView;
     }
 
-    private boolean allPermissionsGranted() {
-        return ContextCompat.checkSelfPermission(
-                requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    private void saveCredentials(String email, String name, SharedPreferences preferences) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("email", email);
+        editor.putString("name", name);
+        editor.apply();
+    }
+    private String[] getCredentials(SharedPreferences preferences) {
+        String email = preferences.getString("email", "");
+        String name = preferences.getString("name", "");
+        return new String[]{email, name};
     }
 }
